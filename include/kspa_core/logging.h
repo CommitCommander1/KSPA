2c4cb71ea1f5da9ec57576b49945148933ae735b
test <linkingt@proton.me>
2025-03-09 23:00:54 -0400

    adding logging 6

--- include/kspa_core/logging.h
+++ include/kspa_core/logging.h
@@ -12,13 +12,14 @@ enum LogLevel { DEBUG, INFO, WARNING, ERROR, CRITICAL};
 
 class Logger {
 public:
-  Logger(const std::string& filename) : filename(filename);
-  ~Logger(){  };
+  Logger(const std::string& filename) : filename(filename){};
+  ~Logger(){};
   void logMessage(const std::string& message);
 private:
   std::string leveltoString(LogLevel level);
   std::ofstream logFile;
   std::string filename;
-};
+    };
+  }
 }
 #endif // !LOGGING_H

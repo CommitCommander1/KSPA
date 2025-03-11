835ee8d6d9c0e802d901ee6e472d2d46dec8534f
test <linkingt@proton.me>
2025-03-11 06:42:00 -0400

    logging time added

--- src/KSPA_core/logging.cpp
+++ src/KSPA_core/logging.cpp
@@ -1,5 +1,6 @@
 #include "kspa_core/logging.h"
 #include <iostream>
+#include <ctime>
 
 namespace kspa_core {
 namespace logging {
@@ -7,5 +8,14 @@ namespace logging {
 void Logger::logMessage(const std::string& message) {
   std::cout<< "[KSPA Core Log]: " << message << std::endl;
 }
+void Logger::log(LogLevel level, const std::string& message) {
+  // Get current time stamp
+  time_t timestamp;
+  time(&timestamp);
+  std::cout << ctime(&timestamp);
+  // Create Log entry
+  // output to console
+  // output to log file
 }
+}     
 }
